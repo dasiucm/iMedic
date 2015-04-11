a00d8e039ec3eb2c1d51bf2b2bf18983787c243a
RotaruDan <drotaru@ucm.es>
2015-04-11 21:16:00 +0200

    Se añaden las reglas y objetivos necesarios para tener una primera versión del caso de uso "Pedir cita".

--- src/icaro/aplicaciones/agentes/AgenteAplicacionDialogoPaciente/tools/conversacionPaciente.java
+++ src/icaro/aplicaciones/agentes/AgenteAplicacionDialogoPaciente/tools/conversacionPaciente.java
@@ -1,5 +1,8 @@
 package icaro.aplicaciones.agentes.AgenteAplicacionDialogoPaciente.tools;
 
 public class conversacionPaciente {
-	public static final String obtenerInfoCita = "Necesitar� los datos de la cita";
+	public static final String obtenerInfoCita1dameDatos = "Facilitame los datos de la cita (una fecha y un médico)";
+	public static final String obtenerInfoCita2fechaRegistrada = "Fecha registrada, ahora queda el médico";
+	public static final String obtenerInfoCita3medicoRegistrado = "Médico registrado, ahora queda la fecha";
+	public static final String obtenerInfoCita4todoCompletado = "Estueendo, �la cita se ha realizado con éxito!";
 }
--- src/icaro/aplicaciones/agentes/AgenteAplicacionDialogoPaciente/tools/tipoNotifPaciente.java
+++ src/icaro/aplicaciones/agentes/AgenteAplicacionDialogoPaciente/tools/tipoNotifPaciente.java
@@ -2,4 +2,6 @@ package icaro.aplicaciones.agentes.AgenteAplicacionDialogoPaciente.tools;
 
 public class tipoNotifPaciente {
 	public static String inicioPeticion = "InicioPeticion";
+	public static String nombre = "nombre";
+	public static String fecha = "fecha";
 }
