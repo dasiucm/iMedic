249ae45a08b71f5a6a9843c8ddd8e81761d479f5
RotaruDan <drotaru@ucm.es>
2015-04-13 13:11:11 +0200

    Arreglado otro pequeño error.

--- src/icaro/aplicaciones/agentes/AgenteAplicacionDialogoPaciente/tools/conversacionPaciente.java
+++ src/icaro/aplicaciones/agentes/AgenteAplicacionDialogoPaciente/tools/conversacionPaciente.java
@@ -4,5 +4,5 @@ public class conversacionPaciente {
 	public static final String obtenerInfoCita1dameDatos = "Facilitame los datos de la cita (una fecha y un médico)";
 	public static final String obtenerInfoCita2fechaRegistrada = "Fecha registrada, ahora queda el médico";
 	public static final String obtenerInfoCita3medicoRegistrado = "Médico registrado, ahora queda la fecha";
-	public static final String obtenerInfoCita4todoCompletado = "Estueendo, �la cita se ha realizado con éxito!";
+	public static final String obtenerInfoCita4todoCompletado = "Estueendo, la cita se ha realizado con éxito!";
 }
