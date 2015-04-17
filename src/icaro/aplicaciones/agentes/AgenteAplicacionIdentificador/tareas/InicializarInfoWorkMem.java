7e682c43bd603a1af849f102762147d0428627f8
Daniel Farina <splitfirex@gmail.com>
2015-04-17 23:37:36 +0200

    Incluyo funcionalidades de foco.

--- src/icaro/aplicaciones/agentes/AgenteAplicacionIdentificador/tareas/InicializarInfoWorkMem.java
+++ src/icaro/aplicaciones/agentes/AgenteAplicacionIdentificador/tareas/InicializarInfoWorkMem.java
@@ -5,12 +5,17 @@
 
 package icaro.aplicaciones.agentes.AgenteAplicacionIdentificador.tareas;
 
-import icaro.aplicaciones.agentes.AgenteAplicacionDialogoCitasCognitivo.objetivos.ObtenerInfoInterlocutor;
+import icaro.aplicaciones.agentes.AgenteAplicacionIdentificador.objetivos.DistribuirMensajes;
+import icaro.aplicaciones.agentes.AgenteAplicacionIdentificador.objetivos.IdentificarUsuario;
+import icaro.aplicaciones.agentes.AgenteAplicacionIdentificador.objetivos.Inactividad;
+import icaro.aplicaciones.agentes.AgenteAplicacionIdentificador.objetivos.InactividadProlongada;
+import icaro.aplicaciones.agentes.AgenteAplicacionIdentificador.objetivos.ObtenerDNIUsuario;
 import icaro.aplicaciones.agentes.AgenteAplicacionIdentificador.objetivos.ObtenerNombreUsuario;
-import icaro.aplicaciones.informacion.gestionCitas.MemUsuario;
-import icaro.infraestructura.entidadesBasicas.procesadorCognitivo.Focus;
 import icaro.infraestructura.entidadesBasicas.procesadorCognitivo.TareaSincrona;
 import icaro.infraestructura.recursosOrganizacion.recursoTrazas.imp.componentes.InfoTraza;
+
+import java.util.ArrayList;
+import java.util.Collection;
 
 /**
  *
@@ -28,6 +33,14 @@ public class InicializarInfoWorkMem extends TareaSincrona {
 			this.getItfConfigMotorDeReglas()
 					.setfactHandlesMonitoring_afterActivationFired_DEBUGGING(
 							true);
+			Collection<Object> objetosAinsertar = new ArrayList<Object>();
+			objetosAinsertar.add(new DistribuirMensajes());
+			objetosAinsertar.add(new IdentificarUsuario());
+			objetosAinsertar.add(new Inactividad());
+			objetosAinsertar.add(new InactividadProlongada());
+			objetosAinsertar.add(new ObtenerDNIUsuario());
+			objetosAinsertar.add(new ObtenerNombreUsuario());
+			this.getEnvioHechos().insertarObjetosEnMiMemoria(objetosAinsertar);
 			//this.getEnvioHechos().insertarHecho(new MemUsuario());
 			//this.getEnvioHechos().insertarHechoWithoutFireRules(new Focus());
 			//this.getEnvioHechos().insertarHecho(new ObtenerInformacionUsuario());
