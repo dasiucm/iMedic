6f54b218516c5427b27306886e044c5c5910fc33
josecgon <josecgonzalezs@gmail.com>
2015-04-30 19:57:16 +0200

    Añadido el recurso calendario
    Añadido el recurso calendario (tiene dos mapas con la misma información,
    uno indexado por el nombre del paciente y otro indexado por el nombre
    del médico para facilitar los accesos y las búsquedas). También he
    modificado la CitaMedica para usar esta clase en el recurso.

--- src/icaro/aplicaciones/informacion/gestionCitas/CitaMedica.java
+++ src/icaro/aplicaciones/informacion/gestionCitas/CitaMedica.java
@@ -1,9 +1,29 @@
 package icaro.aplicaciones.informacion.gestionCitas;
 
 public class CitaMedica {
-	public String usuario;
-	public String nombrePaciente;
-	public String dniPaciente;
+	// public String usuario;
+	UsuarioContexto paciente;
+	// public String nombrePaciente;
+	// public String dniPaciente;
 	public String nombreMedico;
 	public String fecha;
+
+	public CitaMedica(UsuarioContexto paciente, String nombreMedico, String fecha) {
+		this.paciente = paciente;
+		this.nombreMedico = nombreMedico;
+		this.fecha = fecha;
+	}
+
+	public String getMedico() {
+		return this.nombreMedico;
+	}
+
+	public String getFecha() {
+		return this.fecha;
+	}
+
+	public UsuarioContexto getPaciente() {
+		return this.paciente;
+	}
+
 }
