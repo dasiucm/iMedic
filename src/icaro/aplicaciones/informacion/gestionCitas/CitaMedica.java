590c31737720f14ff1be65819ed5cc1412ff119c
josecgon <josecgonzalezs@gmail.com>
2015-05-01 12:24:24 +0200

    Cambiada clase CitaMedica

--- src/icaro/aplicaciones/informacion/gestionCitas/CitaMedica.java
+++ src/icaro/aplicaciones/informacion/gestionCitas/CitaMedica.java
@@ -1,9 +1,28 @@
 package icaro.aplicaciones.informacion.gestionCitas;
 
 public class CitaMedica {
-	public String usuario;
+	
+	/*public String usuario;
 	public String nombrePaciente;
 	public String dniPaciente;
-	public String nombreMedico;
+	public String nombreMedico;*/
+	public UsuarioContexto paciente;
+	public String medico;
 	public String fecha;
+
+	
+	
+	public CitaMedica(UsuarioContexto paciente, String medico, String fecha) {
+		this.paciente = paciente;
+		this.medico = medico;
+		this.fecha = fecha;
+	}
+
+	public String getMedico() {
+		return medico;
+	}
+
+	public String getFecha() {
+		return fecha;
+	}
 }
